a227a2270f42b5b184c1b36f7bfd9f414a33d29d
GibsonSab <sabelogibson1@gmail.com>
2022-02-14 21:53:06 +0000

    Added persisted memory with Room and dagger. Will adjust UI in later releases

---
+++ app/src/main/java/com/sabg/noteapp/data/NoteDatabaseDao.kt
@@ -0,0 +1,30 @@
+package com.sabg.noteapp.data
+
+import androidx.compose.runtime.MutableState
+import androidx.room.*
+import com.sabg.noteapp.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDatabaseDao {
+
+    @Query("SELECT * FROM notes_tbl")
+    fun getNotes(): Flow<List<Note>>
+
+
+    @Query("SELECT * FROM notes_tbl WHERE id =:id")
+    suspend fun getNoteById(id:String):Note
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note:Note)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(note: Note)
+
+    @Query("DELETE FROM notes_tbl")
+    suspend fun deleteAll()
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+}
